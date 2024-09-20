5b0bca814a01107ee4466e256f3d38f55cab3e17
Gaytri Shelke <gayatrishelke093@gmail.com>
2024-09-19 23:21:22 -0700

    committed all

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import LoginPage from "./components/LoginPage";
+import "./styles.css";
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <LoginPage/>
+      
+    </div>
+  );
+}
